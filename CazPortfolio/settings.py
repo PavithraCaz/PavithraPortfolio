db7b2ab402bdcd537f13e1f0d3295a39611229b5
PavithraCaz <pavithracaz@gmail.com>
2023-05-13 14:56:49 +0530

    static files issue resolved

--- CazPortfolio/settings.py
+++ CazPortfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9ii0fgxwox2k0u-fjerqp-c2q3fi-0$k(sduw#yuenv$*2jlfc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
